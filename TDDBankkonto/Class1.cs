f60d169c51360658abcd21ff7bc06d7dd11accc4
Kriistoffer <kjohansson93@hotmail.se>
2021-11-09 19:53:35 +0100

    Uploading project

---
+++ TDDBankkonto/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TDDBankkonto
+{
+    public class BankAccount
+    {
+        public double Balance { get; set; } = 0;
+        public bool HasCredit { get; set; } = false;
+        public double Credit { get; set; } = 1500;
+        public bool HasSwish { get; set; } = false;
+        public double AllowedSwishAmount { get; set; } = 3000;
+        public bool IsFlagged { get; set; } = false;
+        public bool IsSavingsAccount { get; set; } = false;
+        public bool IsInvestmentAccount { get; set; } = false;
+
+        public bool IsTransactionPositive(int deposit)
+        {
+            if (deposit > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DenyTransaction(double withdraw)
+        {
+            if (withdraw > Balance || withdraw <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AccountHasCredit(bool HasCredit)
+        {
+            if (HasCredit)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DenyWithdrawWithCredit(double withdraw)
+        {
+            if (withdraw > (Balance + Credit) || withdraw <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AccountHasSwish(bool HasSwish)
+        {
+            if (HasSwish)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void MakeSwishPayment(double SendAmount)
+        {
+            if (SendAmount < AllowedSwishAmount)
+            {
+                AllowedSwishAmount -= SendAmount;
+            }
+
+        }
+    }
+}
---
+++ TDDBankkontoTests/BankAccountTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDBankkonto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBankkonto.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        [DataRow(1000, true)]
+        [DataRow(50, true)]
+        [DataRow(-100, false)]
+        [DataRow(0, false)]
+        public void IsTransactionPositiveTest(int deposit, bool expected)
+        {
+            var BankAccount = new BankAccount();
+
+            var result = BankAccount.IsTransactionPositive(deposit);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod()]
+        [DataRow(1000, 500, true)]
+        [DataRow(200, 100, true)]
+        [DataRow(10000, 4000, true)]
+        [DataRow(200, 800, false)]
+        [DataRow(0, 10000, true)]
+        [DataRow(-2000, 10000, true)]
+        public void DenyTransactionTest(double withdraw, double balance, bool expected)
+        {
+            var BankAccount = new BankAccount();
+            BankAccount.Balance = balance;
+
+            var result = BankAccount.DenyTransaction(withdraw);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(true, true)]
+        [DataRow(false, false)]
+        public void AccountHasCreditTest(bool HasCredit, bool expected)
+        {
+            var BankAccount = new BankAccount();
+            var result = BankAccount.AccountHasCredit(HasCredit);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(1000, 800, 100, true)]
+        [DataRow(500, 1000, 200, false)]
+        public void DenyWithdrawWithCreditTest(double withdraw, double balance, double credit, bool expected)
+        {
+            var BankAccount = new BankAccount();
+            BankAccount.Balance = balance;
+            BankAccount.Credit = credit;
+
+            var result = BankAccount.DenyWithdrawWithCredit(withdraw);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(false, false)]
+        [DataRow(true, true)]
+        public void AccountHasSwishTest(bool HasSwish, bool expected)
+        {
+            var BankAccount = new BankAccount();
+            BankAccount.HasSwish = HasSwish;
+            var result = BankAccount.AccountHasSwish(HasSwish);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(100, 200, true)]
+        public void MakeSwishPaymentTest(double paymentAmount, double allowedAmount, bool expected)
+        {
+            var BankAccount = new BankAccount();
+            BankAccount.MakeSwishPayment();
+        }
+    }
+}
